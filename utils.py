4a528a03b7893f9a558bd48a9a538630a06d9a92
alexgeleisha <121766149+alexgeleisha@users.noreply.github.com>
2026-02-04 17:32:39 +0300

    Update utils.py
    document add_numbers function

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 # This function returns a greeting message
 def greet(name):
     return f"Hello, {name}!"
-
+    
+# Adds two numbers and returns the result
 def add_numbers(a, b):
     return a + b
